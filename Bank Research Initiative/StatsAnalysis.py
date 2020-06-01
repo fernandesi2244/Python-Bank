a675302162fc05db0c3bbcde6173e32a54bbccbc
Ian Fernandes <34047201+fernandesi2244@users.noreply.github.com>
2020-05-31 22:23:20 -0500

    Add files via upload

---
+++ Bank Research Initiative/StatsAnalysis.py
@@ -0,0 +1,20 @@
+import pandas as pd, matplotlib.pyplot as plt
+from scipy.stats import linregress
+import numpy as nd
+
+class StatsAnalysis(object):
+    def __init__(self, fileName):
+        self.fileName = fileName
+        self.df = pd.read_csv('Bank Research Initiative/'+fileName)
+    
+    def regression(self, x_var, y_var):
+        self.df = self.df[self.df[y_var] != 0]
+        ageBalanceDf = self.df[[x_var,y_var]][:100]
+        ageBalanceDf.plot(kind='scatter',x=x_var,y=y_var,color='red')
+        x = ageBalanceDf.Age
+        y = ageBalanceDf.EstimatedSalary
+        stats = linregress(x,y)
+        m = stats.slope
+        b = stats.intercept
+        plt.plot(x, m*x+b, color="blue")
+        plt.show()
